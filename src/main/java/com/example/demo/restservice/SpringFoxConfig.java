9613ce1c8aba155d0f4f1cdd49fcd031d614ee88
Asha Rampershad <asha.rampershad@fnb.co.za>
2021-05-31 08:06:20 +0200

    My first commit

---
+++ src/main/java/com/example/demo/restservice/SpringFoxConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.restservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+
+import java.util.Collections;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My REST API",
+                "Some custom description of API.",
+                "API TOS",
+                "Terms of service",
+                new Contact("Asha Rampershad", "www.example.com", "myeaddress@company.com"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
+}
